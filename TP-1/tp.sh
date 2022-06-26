788c5662aa3aba83fd3a9636c17a2b298d313deb
Viviana Rondón <viviana.rondon97@gmail.com>
2022-06-26 20:58:56 -0300

    ultimos cambios

--- TP-1/tp.sh
+++ TP-1/tp.sh
@@ -1,34 +1,29 @@
 # a
-# sed 's/\./\.\n/g' breve_historia.txt > breve_historia_punto_a.txt
+sed 's/\./\.\n/g' breve_historia.txt
 
 # b
-# sed '/^\s$/d' breve_historia.txt > breve_historia_punto_b.txt
+sed '/^\s$/d' breve_historia.txt
 
 # c
-# sed -e 's/\./\.\n/g' -e '/^\s$/d' breve_historia.txt > breve_historia_2.txt
+sed -e 's/\./\.\n/g' -e '/^\s$/d' breve_historia.txt > breve_historia_2.txt
 
 # d
-# grep -i ' guerra\b' breve_historia.txt
+grep -i ' guerra\b' breve_historia.txt
 
 # e
-# grep -E '^La.*(s|s\.)$' breve_historia.txt  # preguntar al profe
+grep -E 'La.*(s\b|s\.)' breve_historia.txt
 
 # f
-# grep -c 'peronismo\b' breve_historia.txt
+grep -c 'peronismo\b' breve_historia.txt
 
 # g
-# grep -c -E 'Sarmiento | Rosas' breve_historia.txt
+grep -c -E 'Sarmiento | Rosas' breve_historia.txt
 
-# h SXIX = 1801 - 1900
-# Muestre las oraciones que tengan fechas referidas al siglo XIX.
-# grep -E '180[1-9] | 18[1-9][0-9] | 1900' breve_historia.txt
-# grep -E '18(0[1-9] | [1-9][0-9]) | 1900' breve_historia.txt
+# h
+grep -E '((180[1-9]|18[1-9][0-9]|1900))[[\.|\,]]{0,1}' breve_historia.txt
 
 # i
-# sed -e 's/^\w*\b//g' breve_historia.txt > breve_historia_punto_i.txt
+sed -e 's/^\w*\b//g' breve_historia.txt
 
 # j
 ls | grep -c '.txt'
-
-# --> consultar si el conteo es por palabra o por aparición en oración/párrafo
-# --> consultar por punto h
